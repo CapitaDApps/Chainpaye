eae4aa2282b36de612d19ad1a15cc6635c298f4d
kcibdev <brightisaac80@gmail.com>
2026-02-05 11:00:58 +0100

    now push yo staging

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   apps: [
     {
+      // PRODUCTION CONFIG
       name: "chainpaye-prod",
       script: "./dist/app.js",
       env: {
         NODE_ENV: "production",
+        PORT: 3000,
       },
       restart_delay: 5000,
       max_restarts: 10,
@@ -18,6 +20,20 @@ module.exports = {
       args: ["start"],
       env: {
         NODE_ENV: "development",
+        PORT: 3000,
+      },
+      restart_delay: 5000,
+      max_restarts: 10,
+      autorestart: true,
+    },
+    {
+      // STAGING CONFIG
+      name: "chainpaye-staging",
+      script: "pnpm",
+      args: "start", // This runs 'pnpm start'
+      env: {
+        NODE_ENV: "development",
+        PORT: 3001,
       },
       restart_delay: 5000,
       max_restarts: 10,
